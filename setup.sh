45159cd6345596f2fff41f74d3b5626bf286fd02
ritikprajapat21 <ritikprajapati084@gmail.com>
2025-07-09 00:03:38 +0530

    Setup script for system

---
+++ setup.sh
@@ -0,0 +1,25 @@
+#! bin/bash
+
+# Setting up Neovim
+echo "Cloning nvim git repo"
+git clone https://github.com/ritikprajapat21/NvChadConfig ~/.config/nvim
+cd ~/.config/nvim
+git checkout lazyvim
+
+# Setting ghostty
+echo "Cloning ghostty repo"
+git clone https://github.com/ritikprajapat21/ghostty-config ~/.config/ghostty
+cd ~
+
+# Github ssh setup
+echo "Enter your email for ssh key generation:"
+read email
+
+ssh-keygen -t ed25519 -C $email
+
+eval "$(ssh-agent -s)"
+
+ssh-add ~/.ssh/id_ed25519
+
+echo "Your ssh public key:"
+cat ~/.ssh/id_ed25519.pub
